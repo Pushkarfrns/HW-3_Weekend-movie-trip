c315e9c24716d32a647d289b3d6c4a055c5b2748
Pushkarfrns <pushkarfrns@gmail.com>
2018-10-02 16:15:23 -0500

    notebook code in .py format

--- src/Weekend_movie_trip.py
+++ src/Weekend_movie_trip.py
@@ -46,39 +46,39 @@ df_ratings.groupby(['userId']).count()
 df_ratings.groupby(['userId']).count()['movieId']
 
 
-# In[35]:
+# In[231]:
 
 
 df_ratings_max_userid=df_ratings.groupby(['userId']).count()['movieId']
 
 
-# In[36]:
+# In[232]:
 
 
 df_ratings_max_userid= df_ratings_max_userid.to_frame()
 df_ratings_max_userid
 
 
-# In[37]:
+# In[233]:
 
 
 df_ratings_max_userid=df_ratings_max_userid.sort_values('movieId',ascending=False)
 
 
-# In[38]:
+# In[234]:
 
 
 # userid with 414 gace maximum times(movies rating) i.e. gave 2698 movies rating
 df_ratings_max_userid
 
 
-# In[41]:
+# In[235]:
 
 
 df_ratings_max_userid = df_ratings_max_userid.rename(columns={'movieId': 'Number of movies rated'})
 
 
-# In[42]:
+# In[236]:
 
 
 df_ratings_max_userid
@@ -349,20 +349,330 @@ df_ratings.drop('timestamp' ,axis=1,inplace=True)
 df_ratings
 
 
-# In[128]:
+# In[179]:
 
 
 df_Merged_tags_ratings= pd.merge(df_ratings,df_tags, on=["movieId","userId"], how ="outer")
 
 
-# In[129]:
+# In[180]:
 
 
 df_Merged_tags_ratings
 
 
-# In[130]:
+# In[181]:
 
 
-df_Merged_tags_ratings.dropna()
+df_Merged_tags_ratings = df_Merged_tags_ratings.dropna()
+print(df_Merged_tags_ratings.shape)
+print(list(df_Merged_tags_ratings.columns))
+
+
+# In[182]:
+
+
+df_Merged_tags_ratings
+
+
+# In[ ]:
+
+
+
+
+
+# In[135]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# In[183]:
+
+
+df_Merged_tags_ratings.dtypes
+
+
+# In[184]:
+
+
+df_Merged_tags_ratings["tag"] = df_Merged_tags_ratings["tag"].astype('category')
+df_Merged_tags_ratings.dtypes
+df_Merged_tags_ratings["tag_cat"] = df_Merged_tags_ratings["tag"].cat.codes
+df_Merged_tags_ratings.head()
+
+
+# In[170]:
+
+
+print(df_Merged_tags_ratings.shape)
+print(list(df_Merged_tags_ratings.columns))
+
+
+# In[171]:
+
+
+df_Merged_tags_ratings
+
+
+# In[187]:
+
+
+
+df_Merged_tags_ratings_new.shape
+
+
+# In[186]:
+
+
+df_Merged_tags_ratings_new = df_Merged_tags_ratings.filter(['userId','movieId','rating','tag_cat'], axis=1)
+
+
+# In[188]:
+
+
+df_Merged_tags_ratings_new.shape
+
+
+# In[191]:
+
+
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+Y=df_Merged_tags_ratings_new.movieId #output label
+X=df_Merged_tags_ratings_new.drop('movieId',axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2)
+print(X_train)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+#print("Accuracy:",metrics.accuracy_score(y_test, y_train))
+predictions = knn.predict(X_test)
+score = knn.score(X_test, y_test)
+print(score)
+
+
+# In[192]:
+
+
+df_Merged_tags_ratings_new_withTitle= pd.merge(df_Merged_tags_ratings_new,df_movies, on=["movieId"], how ="outer")
+
+
+# In[194]:
+
+
+df_Merged_tags_ratings_new_withTitle.dropna()
+
+
+# In[195]:
+
+
+df_Merged_tags_ratings_new_withTitle.dtypes
+
+
+# In[196]:
+
+
+df_Merged_tags_ratings_new_withTitle["title"] = df_Merged_tags_ratings_new_withTitle["title"].astype('category')
+df_Merged_tags_ratings_new_withTitle.dtypes
+df_Merged_tags_ratings_new_withTitle["title_cat"] = df_Merged_tags_ratings_new_withTitle["title"].cat.codes
+df_Merged_tags_ratings_new_withTitle.head()
+
+df_Merged_tags_ratings_new_withTitle["genres"] = df_Merged_tags_ratings_new_withTitle["genres"].astype('category')
+df_Merged_tags_ratings_new_withTitle.dtypes
+df_Merged_tags_ratings_new_withTitle["genres_cat"] = df_Merged_tags_ratings_new_withTitle["genres"].cat.codes
+df_Merged_tags_ratings_new_withTitle.head()
+
+
+# In[201]:
+
+
+df_Merged_tags_ratings_new_withTitle.shape
+
+
+# In[198]:
+
+
+df_Merged_tags_ratings_new_withTitleFinal = df_Merged_tags_ratings_new_withTitle.filter(['userId','movieId','rating','tag_cat','title_cat','genres_cat'], axis=1)
+
+
+# In[203]:
+
+
+df_Merged_tags_ratings_new_withTitleFinal.dropna()
+
+
+# In[215]:
+
+
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+Y=df_Merged_tags_ratings_new_withTitleFinal.title_cat #output label
+X=df_Merged_tags_ratings_new_withTitleFinal.drop('title_cat',axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2)
+print(X_train)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+#print("Accuracy:",metrics.accuracy_score(y_test, y_train))
+predictions = knn.predict(X_test)
+score = knn.score(X_test, y_test)
+print(score)
+
+
+# In[209]:
+
+
+df_Merged_tags_ratings_new_withTitleFinal.shape
+
+
+# In[208]:
+
+
+df_Merged_tags_ratings_new_withTitleFinal=df_Merged_tags_ratings_new_withTitleFinal.dropna()
+
+
+# In[216]:
+
+
+import seaborn as sns
+
+Var_Corr = df_Merged_tags_ratings_new_withTitleFinal.corr()
+# plot the heatmap and annotation on it
+sns.heatmap(Var_Corr, xticklabels=Var_Corr.columns, yticklabels=Var_Corr.columns, annot=True)
+
+
+# In[217]:
+
+
+corr = df_Merged_tags_ratings_new_withTitleFinal.corr()
+sns.heatmap(corr, 
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values)
+
+
+# In[218]:
+
+
+corr.style.background_gradient().set_precision(2)
+
+
+# In[219]:
+
+
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+Y=df_Merged_tags_ratings_new_withTitleFinal.title_cat #output label
+X=df_Merged_tags_ratings_new_withTitleFinal.drop('title_cat',axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2)
+print(X_train)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+#print("Accuracy:",metrics.accuracy_score(y_test, y_train))
+predictions = knn.predict(X_test)
+score = knn.score(X_test, y_test)
+print(score)
+
+
+# In[237]:
+
+
+df_ratings_max_userid['userId'] = df_ratings_max_userid.index.tolist()
+
+df_ratings_max_userid.index = np.arange(0,len(df_ratings_max_userid))
+
+
+# In[240]:
+
+
+df_ratings_max_userid['userId'] = df_ratings_max_userid.index.tolist()
+
+df_ratings_max_userid.index = np.arange(0,len(df_ratings_max_userid))
+
+
+# In[241]:
+
+
+df_ratings_max_userid
+
+
+# In[244]:
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(15,15))
+ax= sns.barplot(x='Number of movies rated',y='userId',data=df_ratings_max_userid, order = df_ratings_max_userid['userId'])
+ax.set(xlabel='Number of movies rated', ylabel='userId')
+plt.show()
+
+
+# In[243]:
+
+
+df_ratings_max_userid.dtypes
+
+
+# In[245]:
+
+
+df_tag_max_userid['userId'] = df_tag_max_userid.index.tolist()
+
+df_tag_max_userid.index = np.arange(0,len(df_tag_max_userid))
+
+
+# In[246]:
+
+
+df_tag_max_userid
+
+
+# In[248]:
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(20,20))
+ax= sns.barplot(x='Number of movies tagged',y='userId',data=df_tag_max_userid, order = df_tag_max_userid['userId'])
+ax.set(xlabel='Number of movies tagged', ylabel='userId')
+plt.show()
+
+
+# In[249]:
+
+
+df_Merged['userId'] = df_Merged.index.tolist()
+
+df_Merged.index = np.arange(0,len(df_Merged))
+
+
+# In[250]:
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(20,20))
+ax= sns.barplot(x='Number of movies tagged',y='Numberof movies rated',data=df_Merged, order = df_Merged['Numberof movies rated'])
+ax.set(xlabel='Number of movies tagged', ylabel='Numberof movies rated')
+plt.show()
+
+
+# In[257]:
+
+
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+Y=df_Merged_tags_ratings_new_withTitleFinal.title_cat #output label
+X=df_Merged_tags_ratings_new_withTitleFinal.drop('title_cat',axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2)
+print(X_train)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+#print("Accuracy:",metrics.accuracy_score(y_test, y_train))
+predictions = knn.predict(X_test)
+score = knn.score(X_test, y_test)
+print(score)
 
